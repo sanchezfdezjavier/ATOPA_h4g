07103f173f3f81389a2b2f1b66c8fc3373dec5f5
sanchezfdezjavier <32944505+sanchezfdezjavier@users.noreply.github.com>
2018-03-09 23:18:34 +0100

    Add files via upload
    Read funcionando pero no completo, hay que adaptar el google forms al formato que queremos hacer.

--- src/es/upm/h4g/atopa/Read.java
+++ src/es/upm/h4g/atopa/Read.java
@@ -2,22 +2,33 @@ package es.upm.h4g.atopa;
 
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.*;
 
 public class Read {
+
+    private HashMap<String, List<String>> excelResult = new HashMap<>();
+
+    public Read(File file) {
+
+    }
+
     public static void main(String[] args) {
         try {
 
-            FileInputStream file = new FileInputStream(new File("testExcel.xls"));
+            FileInputStream file = new FileInputStream(new File("/Users/javisanchez/desktop/test_excel3.xls"));
             HSSFWorkbook workbook = new HSSFWorkbook(file);
             HSSFSheet sheet = workbook.getSheetAt(0);
             Iterator<Row> rowIterator = sheet.iterator();
             rowIterator.next();
+            
             while (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
                 //For each row, iterate through each columns
@@ -26,13 +37,16 @@ public class Read {
                     Cell cell = cellIterator.next();
                     switch (cell.getCellType()) {
                         case Cell.CELL_TYPE_BOOLEAN:
-                            System.out.println("boolean===>>>" + cell.getBooleanCellValue() + "\t");
+                            System.out.println("boolean ==>" + cell.getBooleanCellValue() + "\t");
+                            cell.getBooleanCellValue();
                             break;
                         case Cell.CELL_TYPE_NUMERIC:
-                            System.out.println("numeric===>>>" + cell.getNumericCellValue() + "\t");
+                            System.out.println("numeric ==>" + cell.getNumericCellValue() + "\t");
+                            cell.getNumericCellValue();
                             break;
                         case Cell.CELL_TYPE_STRING:
-                            System.out.println("String===>>>" + cell.getStringCellValue() + "\t");
+                            System.out.println("String ==>" + cell.getStringCellValue() + "\t");
+                            cell.getStringCellValue();
                             break;
                     }
                 }
@@ -47,5 +61,3 @@ public class Read {
     }
 
 }
-
-
