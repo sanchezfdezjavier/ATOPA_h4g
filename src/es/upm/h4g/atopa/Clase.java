da57ada4fcbf950ea4e1d4a333cc11b988ddcebb
danagorn <37210935+danagorn@users.noreply.github.com>
2018-03-09 20:06:21 +0100

    Add files via upload

--- src/es/upm/h4g/atopa/Clase.java
+++ src/es/upm/h4g/atopa/Clase.java
@@ -1,29 +1,37 @@
+/**
+ * 
+ */
 package es.upm.h4g.atopa;
-
-
-
+import java.util.*;
+/**
+ * @author Daniel del Riego San Martín
+ *
+ */
 public class Clase {
-    private Map<Integer, Alumno> alumnos;
-    private String nombre;//Rollo 1c y eso
+	private Map<Integer, Alumno> alumnosmap;
+	private String nombre;//Rollo 1c y eso
+	
+	public Clase(List<Alumno> alumnos, String nombre){
+		this.alumnosmap = new HashMap<>();
+		for (Alumno a: alumnos){
+			this.alumnosmap.put(a.getNumero(), a);
+			this.nombre = nombre;
+		}
+	}
 
-    public Clase(List<Alumno> alumnos, String nombre){
-        this.alumnos = new HashMap<ðŸ˜ );
-        for (Alumno a: alumnos){
-            this.alumnos.put(a.getNumero(), a);
-            this.nombre = nombre;
-        }
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Alumno getAlumno(int n){
-        return alumnos.get(n);
-    }
-
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public Alumno getAlumno(int n){
+		return alumnosmap.get(n);
+	}
+	public List<Alumno> getAlumnos(){
+		List<Alumno> alumnos = new ArrayList<>(alumnosmap.values());
+		return alumnos;
+	}
 }
